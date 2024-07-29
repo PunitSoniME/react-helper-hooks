20eea34e4b820a3581ac0d5dd6c91891a595d161
Punit Soni <punit.soni33@gmail.com>
2024-07-29 15:33:40 +0530

    vite config reverted

--- example/vite.config.js
+++ example/vite.config.js
@@ -4,7 +4,6 @@ import ReactPlugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [ReactPlugin({})],
-  base: '/react-helper-hooks/',  //  This is required when using Github Pages
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
